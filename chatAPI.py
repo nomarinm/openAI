36827d069bde14f9b068b22af3e6f1eefba439fc
Norbey Marin <nomarinm@gmail.com>
2024-03-17 23:01:18 -0500

    add servicio al cliente

--- chatAPI.py
+++ chatAPI.py
@@ -9,8 +9,11 @@ from openai import OpenAI
 
 from dotenv import load_dotenv
 load_dotenv()
-path_file = "iso9001.csv"
-datos = pd.read_csv(path_file)
+path_file1 = "iso9001.csv"
+datos_ISO = pd.read_csv(path_file1)
+
+path_file2 = "FDSA.csv"
+datos_SERV = pd.read_csv(path_file2)
 client = OpenAI(
     organization= os.getenv('ORGANIZATION'),
     api_key= os.getenv('API_KEY')
@@ -42,32 +45,62 @@ def inicio():
 @app.post("/consulta")
 def procesar(msj:Msj):
     mens = msj.dict()
-    prompt = mens["mensaje"]
-    dat = buscar(prompt, datos, 2)
-    embed = dat['texto'].str.cat()  # aqui estan concatenados tos los embeddings
+    if mens["type"] == "ISO":
+        prompt = mens["mensaje"]
+        dat = buscar(prompt, datos_ISO, 2)
+        embed = dat['texto'].str.cat()  # aqui estan concatenados tos los embeddings
 
-    query = f""" Usa la siguiente información para contestar la pregunta, si no encuentras la 
-    respuesta, contesta amablemente diciendo que en el momento no dispones de la información solicitada, en caso de que 
-    la pregunta sea un saludo debes contestar amablemente al saludo indicando tu nombre y el de tu creador"
-    información:
-    \"\"\"
-    {embed}
-    \"\"\"
-    pregunta:
-    \"\"\"
-    {prompt}
-    \"\"\" 
-    """
-    rol = "Eres EMMA creada por Ingenieros de InproExt, un asistente experto en la norma iso 9001. Tu objetivo es proporcionar respuestas claras y precisas, con un límite máximo de 70 palabras"
-    stream = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": rol},
-            {"role": "user", "content": query}],
-        max_tokens=400,
-        temperature=0.8,
-        #stream=True,
-    )
-    print(stream.choices[0].message.content)
+        query = f""" Usa la siguiente información para contestar la pregunta, si no encuentras la 
+        respuesta, contesta amablemente diciendo que en el momento no dispones de la información solicitada, en caso de que 
+        la pregunta sea un saludo debes contestar amablemente al saludo indicando tu nombre y el de tu creador"
+        información:
+        \"\"\"
+        {embed}
+        \"\"\"
+        pregunta:
+        \"\"\"
+        {prompt}
+        \"\"\" 
+        """
+        rol = "Eres EMMA creada por Ingenieros de InproExt, un asistente experto en la norma iso 9001. Tu objetivo es proporcionar respuestas claras y precisas, con un límite máximo de 70 palabras"
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": rol},
+                {"role": "user", "content": query}],
+            max_tokens=400,
+            temperature=0.8,
+            #stream=True,
+        )
+        print(stream.choices[0].message.content)
 
-    return [{"type":"ISO","mensaje":stream.choices[0].message.content}]
+        return [{"type":"ISO","mensaje":stream.choices[0].message.content}]
+    if mens["type"] == "SERVICIO":
+        prompt = mens["mensaje"]
+        dat = buscar(prompt, datos_SERV, 2)
+        embed = dat['texto'].str.cat()  # aqui estan concatenados tos los embeddings
+        query = f""" Usa la siguiente información para contestar la pregunta, si no encuentras la 
+        respuesta, contesta amablemente diciendo que en el momento no dispones de la información solicitada, en caso de que 
+        la pregunta sea un saludo debes contestar amablemente al saludo indicando tu nombre y el de tu creador"
+        información:
+        \"\"\"
+        {embed}
+        \"\"\"
+        pregunta:
+        \"\"\"
+        {prompt}
+        \"\"\" 
+        """
+        rol = "Eres EMMA, creada por Ingenieros de InproExt, un asistente experto en servicio al cliente. Tu objetivo es proporcionar respuestas claras y precisas, con un límite máximo de 50 palabras"
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": rol},
+                {"role": "user", "content": query}],
+            max_tokens=400,
+            temperature=0.8,
+            #stream=True,
+        )
+        print(stream.choices[0].message.content)
+
+        return [{"type": "SERVICIO", "mensaje": stream.choices[0].message.content}]
